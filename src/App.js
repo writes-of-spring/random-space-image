a2af7779afcbc1f673370fe42d4609a0ffdd78d5
Peter Kerins <pete.kerins@gmail.com>
2019-11-02 12:30:17 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react'
+import styled from 'styled-components'
+
+import './App.css'
+const Main = styled.main`
+  min-height: 100vh;
+  background-color: #333333;
+  background-image: url(${props => props.bgImg});
+  background-size: cover;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [image, setImage] = useState(null)
+  const initialRender = useRef(true)
+
+  useEffect(() => {
+    if (initialRender.current) {
+      initialRender.current = false
+      console.log('First Run')
+      return
+    }
+    if (query) {
+      async function fetchData() {
+        const result = await fetch(
+          `https://images-api.nasa.gov/search?q=${query}`
+        )
+        const response = await result.json()
+        const { items } = response.collection
+        const randomImage = Math.floor(Math.random() * items.length + 1)
+        console.log(randomImage)
+        if (items.length) {
+          setImage(items[randomImage].links[0].href)
+        }
+      }
+
+      fetchData()
+    }
+  }, [query])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Main bgImg={image}>
+      <section id="search">
+        <input
+          key="searchBox"
+          id="search-field"
+          onChange={e => setQuery(e.target.value)}
+          type="text"
+          placeholder="type here"
+          value={query}
+        />
+      </section>
+    </Main>
+  )
 }
 
-export default App;
+export default App
