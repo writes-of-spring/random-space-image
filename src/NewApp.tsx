c0e3116ab709c2e48dfbbc3bb7bf7b4726400c0a
Peter Kerins <pete.kerins@gmail.com>
2023-02-19 08:55:53 +0000

    use rhf

--- src/NewApp.tsx
+++ src/NewApp.tsx
@@ -1,38 +1,61 @@
 import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import "./App.css";
+import { useForm } from "react-hook-form";
 
 async function fetchImage(query: string): Promise<APIRes> {
-  const result = await fetch(`https://images-api.nasa.gov/search?q=${query}`);
+  const url = new URL("https://images-api.nasa.gov/search");
+  url.searchParams.append("q", query);
+  url.searchParams.append("media_type", "image");
+
+  const result = await fetch(url.toString());
   return await result.json();
 }
-const getRandomImage = (data: APIRes) => {
+const getRandomImage = (data?: APIRes) => {
+  if (!data?.collection?.items?.length) return;
   const randomIndex = Math.floor(Math.random() * data.collection.items.length);
   return data.collection?.items[randomIndex]?.links[0]?.href;
 };
 
-export function App() {
-  const [query, setQuery] = useState("");
+type Form = {
+  searchTerm: string;
+};
 
-  const { data } = useQuery(["image", query], () => fetchImage(query), {
-    enabled: !!query,
-    select: getRandomImage,
+export function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const { register, handleSubmit } = useForm<Form>({
+    defaultValues: {
+      searchTerm: "",
+    },
+  });
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["image", searchQuery],
+    queryFn: () => fetchImage(searchQuery),
+    enabled: !!searchQuery,
+    keepPreviousData: true,
   });
   console.log(data);
+
+  const onSubmit = (data: Form) => {
+    setSearchQuery(data.searchTerm);
+  };
   return (
     <main
       className="h-screen bg-gray-800 flex items-center justify-center flex-wrap bg-cover"
-      style={{ backgroundImage: `url(${data})` }}
+      style={{ backgroundImage: `url(${getRandomImage(data)})` }}
     >
       <section className="container mx-auto">
-        <input
-          key="searchBox"
-          id="search-field"
-          onChange={(e) => setQuery(e.target.value)}
-          type="text"
-          placeholder="search..."
-          value={query}
-        />
+        <form onSubmit={handleSubmit(onSubmit)} onBlur={handleSubmit(onSubmit)}>
+          <input
+            key="searchBox"
+            type="text"
+            id="search-field"
+            placeholder="search and hit enter"
+            {...register("searchTerm")}
+            className="bg-gray-200 rounded-full w-full px-4 py-1 text-gray-700 focus:outline-none focus:shadow-outline"
+          />
+        </form>
       </section>
     </main>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./NewApp";
-import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
